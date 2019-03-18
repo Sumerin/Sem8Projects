cccf708d9cf285c91da104a6beebf0ebdda8c93a
Sumerin <mikolaj.sumowski@gmail.com>
2019-03-18 21:02:40 +0100

    remove

--- ScripLang/Zad2/Setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#  -*- coding: utf-8 -*-
-from distutils.core import setup, Extension
-
-module1 = Extension('simple_graphs',
-                    sources = ['SimpleGraph.c'])
-
-setup (name = 'PackageName',
-       ext_modules = [module1])
--- ScripLang/Zad2/SetupT.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('hello',
-                    sources = ['t.c'])
-
-setup (name = 'PackagdaesaName',
-       version = '1.1',
-       description = 'This is a demo package',
-       ext_modules = [module1])
